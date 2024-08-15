02f29943a40a5a2f64702cf3413b69295c6539c8
ai-doge <123085890+ai-doge@users.noreply.github.com>
2024-08-15 22:59:21 +0800

    add Package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "sam2",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "sam2",
             targets: ["sam2"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.1.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "sam2"),
+            name: "sam2",
+            dependencies: [.product(name: "MLX", package: "mlx-swift")]),
         .testTarget(
             name: "sam2Tests",
             dependencies: ["sam2"]),
